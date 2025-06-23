237d8fc8dda96b5c8c3eba67dd1ec5f6f9db6924
Nitin-Lohumi <lohuminitin@gmail.com>
2025-06-23 12:31:00 +0530

    add

--- controlers/ManualLogincontroller.js
+++ controlers/ManualLogincontroller.js
@@ -9,23 +9,25 @@ const ManualLogin = async (req, res) => {
     if (!user) {
       return res.status(401).json({ msg: "User not found", status: false });
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
-
     const token = jwt.sign(
       { _id: user._id, email: user.email, name: user.name },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
-
+    if (!user.picture) {
+      user.picture = "../public/pngwing.com (12).png";
+      user.save();
+    }
+    console.log(user.picture);
     res.status(200).json({
       msg: "Login successful",
       status: true,
       token,
-      user: { id: user._id, name: user.name, email: user.email },
+      user: { id: user._id, name: user.name, email: user.email, picture: user.picture },
     });
   } catch (error) {
     console.error("Login error:", error);
