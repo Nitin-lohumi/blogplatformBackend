7b79f24af63a58e6263728dbb56bf220a8e168ef
Nitin-Lohumi <lohuminitin@gmail.com>
2025-06-23 11:34:30 +0530

    add

--- middleware/ValidateJWT.js
+++ middleware/ValidateJWT.js
@@ -2,16 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
-
+  console.log(authHeader);
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ msg: "No token provided" });
   }
-
   const token = authHeader.split(" ")[1];
-
+  console.log(token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; 
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ msg: "Invalid token" });
