8fb80f98eeb6cdc9ca542ed8577cd3f1dac9b397
Nitin-Lohumi <lohuminitin@gmail.com>
2025-06-23 13:15:41 +0530

    Add

--- controlers/ManualLogincontroller.js
+++ controlers/ManualLogincontroller.js
@@ -5,29 +5,39 @@ const User = require("../model/user_model");
 const ManualLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(401).json({ msg: "User not found", status: false });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ msg: "Invalid credentials", status: false });
     }
+
+    // ✅ Set default profile picture if not present
+    if (!user.picture) {
+      user.picture = "/pngwing.com (12).png"; // access from public folder in frontend
+      await user.save(); // don't forget to await
+    }
+
     const token = jwt.sign(
       { _id: user._id, email: user.email, name: user.name },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
-    if (!user.picture) {
-      user.picture = "../public/pngwing.com (12).png";
-      user.save();
-    }
-    console.log(user.picture);
+
     res.status(200).json({
       msg: "Login successful",
       status: true,
       token,
-      user: { id: user._id, name: user.name, email: user.email, picture: user.picture },
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+      },
     });
   } catch (error) {
     console.error("Login error:", error);
--- controlers/getThisUser.js
+++ controlers/getThisUser.js
@@ -1,7 +1,7 @@
 const User = require("../model/user_model");
 const getThisUser =async(req,res)=>{
      try {
-        const postId =req.params.id;
+        const postId = req.params.id;
         const user = await User.findOne({_id:postId});
         res.status(200).json({data:user});
      } catch (error) {
